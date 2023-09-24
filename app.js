b318cf31b303f780e7fdb87adc9bbc956a038f87
garvitgabgab <108992700+garvitgabgab@users.noreply.github.com>
2023-09-24 23:13:20 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // importing required packages
 const express = require('express');
 const bodyParser = require('body-parser');
-const db = require('./config/mongoose');
+const db = require('./config/mongoose.js');
 
 // initializing express
 const app = express();
@@ -10,7 +10,7 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 
 // using routes
-app.use('/products', require('./routes/products'));
+app.use('/products', require('./routes/products.js'));
 
 // starting the server
 app.listen(3000, function () {
